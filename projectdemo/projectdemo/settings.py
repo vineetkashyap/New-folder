3a1c1ba1647f8f0b24528cb0952084fb5e0199f4
vineetkashyap <vkvineet66@gmail.com>
2021-07-11 12:54:32 +0530

    first

--- projectdemo/projectdemo/settings.py
+++ projectdemo/projectdemo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dw^mr@nil2(o5$ucd@-3f%5l(x0oulb*xxs2^82a9dqphun$#+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app'
+    'app',
 ]
 
 MIDDLEWARE = [
